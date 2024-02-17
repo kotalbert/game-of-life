21f38fa18f5cb92cd7c402b8f936b5c8bf7d6fa5
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2024-02-17 19:19:37 +0100

    WiP on counting neighbors

--- main.go
+++ main.go
@@ -8,9 +8,10 @@ import (
 )
 
 type Game struct {
-	seed  int
-	n     int
-	cells [][]bool
+	seed        int
+	n           int
+	generations int
+	cells       [][]bool
 }
 
 func (g Game) ToString() any {
@@ -30,31 +31,83 @@ func (g Game) ToString() any {
 
 }
 
-func NewGame(seed int, n int) *Game {
+// NextGeneration creates a new generation of cells
+//
+//	Create new game object with new cells
+func (g Game) NextGeneration() *Game {
 
-	g := &Game{seed: seed, n: n}
+	newCells := createEmptyCells(g.n)
+
+	for i := 0; i < g.n; i++ {
+		for j := 0; j < g.n; j++ {
+			neighbors := g.countNeighbors(i, j)
+			switch neighbors {
+			case 2: // cell lives if alive, or stays dead
+				newCells[i][j] = g.cells[i][j]
+			case 3: // cell is reborn if dead or lives on
+				newCells[i][j] = true
+			default: // cell dies
+				newCells[i][j] = false
+			}
+		}
+
+	}
+	g.cells = newCells
+	return &g
+}
+
+func (g Game) countNeighbors(i int, j int) int {
+	// todo: implement
+	return i + j
+}
+
+func NewGame(seed int, n int, generations int) *Game {
+
+	game := &Game{seed: seed, n: n, generations: generations}
 
 	rand.Seed(int64(seed))
 
-	g.cells = make([][]bool, n)
+	game.cells = make([][]bool, n)
 
+	initializeUniverse(n, game)
+
+	return game
+}
+
+func createEmptyCells(n int) [][]bool {
+	cells := make([][]bool, n)
 	for i := 0; i < n; i++ {
-		g.cells[i] = make([]bool, n)
+		cells[i] = make([]bool, n)
+	}
+	return cells
+}
+
+// initializeUniverse creates a 2D slice of bool,
+//
+//	the initial state of the universe
+func initializeUniverse(n int, game *Game) {
+
+	game.cells = createEmptyCells(n)
+	for i := 0; i < n; i++ {
 		for j := 0; j < n; j++ {
 			// 50% chance of be, if rolls 1 then "alive"
-			g.cells[i][j] = rand.Intn(2) == 1
+			game.cells[i][j] = rand.Intn(2) == 1
 		}
 	}
-
-	return g
 }
 
 func main() {
 
 	n := readInt()
 	seed := readInt()
-	game := NewGame(seed, n)
-	fmt.Println(game.ToString())
+	generations := readInt()
+	game := NewGame(seed, n, generations)
+
+	for i := 0; i < game.generations; i++ {
+		fmt.Println(game.ToString())
+		game = game.NextGeneration()
+
+	}
 
 }
 
