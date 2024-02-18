46720c4414a51518db61fbd71e7eb103785f253b
Pawel Daniluk <pawel.daniluk@kotalbert.net>
2024-02-18 21:08:16 +0100

    Add generations feature (#1)
    * Add counting of neighbours
    * Implement new wrapping around edges

--- main.go
+++ main.go
@@ -8,12 +8,13 @@ import (
 )
 
 type Game struct {
-	seed  int
-	n     int
-	cells [][]bool
+	seed        int
+	n           int
+	generations int
+	cells       [][]bool
 }
 
-func (g Game) ToString() any {
+func (g Game) ToString() string {
 
 	var bld strings.Builder
 	for i := 0; i < g.n; i++ {
@@ -30,30 +31,102 @@ func (g Game) ToString() any {
 
 }
 
-func NewGame(seed int, n int) *Game {
+// NextGeneration creates a new generation of cells
+//
+//	Create new game object with new cells
+func (g Game) NextGeneration() *Game {
 
-	g := &Game{seed: seed, n: n}
+	newCells := createEmptyCells(g.n)
+
+	for i := 0; i < g.n; i++ {
+		for j := 0; j < g.n; j++ {
+			neighbors := g.countNeighbors(i, j)
+			switch neighbors {
+			case 2: // cell lives if alive, or stays dead
+				newCells[i][j] = g.cells[i][j]
+			case 3: // cell is reborn if dead or lives on
+				newCells[i][j] = true
+			default: // cell dies
+				newCells[i][j] = false
+			}
+		}
+
+	}
+	g.cells = newCells
+	return &g
+}
+
+func (g Game) countNeighbors(i int, j int) int {
+
+	// wrap around the edges, flip to the other side if out of bounds
+	nw := g.cells[(i-1+g.n)%g.n][(j-1+g.n)%g.n]
+	no := g.cells[(i-1+g.n)%g.n][j]
+	ne := g.cells[(i-1+g.n)%g.n][(j+1)%g.n]
+	we := g.cells[i][(j-1+g.n)%g.n]
+	ea := g.cells[i][(j+1)%g.n]
+	sw := g.cells[(i+1)%g.n][(j-1+g.n)%g.n]
+	so := g.cells[(i+1)%g.n][j]
+	se := g.cells[(i+1)%g.n][(j+1)%g.n]
+
+	neighbours := []bool{
+		nw, no, ne,
+		we, ea,
+		sw, so, se,
+	}
+	var n int
+	for _, v := range neighbours {
+		if v {
+			n++
+		}
+	}
+	return n
+}
+
+func NewGame(seed int, n int, generations int) *Game {
+
+	game := &Game{seed: seed, n: n, generations: generations}
 
 	rand.Seed(int64(seed))
 
-	g.cells = make([][]bool, n)
+	game.cells = make([][]bool, n)
 
+	initializeUniverse(n, game)
+
+	return game
+}
+
+func createEmptyCells(n int) [][]bool {
+	cells := make([][]bool, n)
 	for i := 0; i < n; i++ {
-		g.cells[i] = make([]bool, n)
+		cells[i] = make([]bool, n)
+	}
+	return cells
+}
+
+// initializeUniverse creates a 2D slice of bool,
+//
+//	the initial state of the universe
+func initializeUniverse(n int, game *Game) {
+
+	game.cells = createEmptyCells(n)
+	for i := 0; i < n; i++ {
 		for j := 0; j < n; j++ {
 			// 50% chance of be, if rolls 1 then "alive"
-			g.cells[i][j] = rand.Intn(2) == 1
+			game.cells[i][j] = rand.Intn(2) == 1
 		}
 	}
-
-	return g
 }
 
 func main() {
 
 	n := readInt()
 	seed := readInt()
-	game := NewGame(seed, n)
+	generations := readInt()
+	game := NewGame(seed, n, generations)
+
+	for i := 1; i <= game.generations; i++ {
+		game = game.NextGeneration()
+	}
 	fmt.Println(game.ToString())
 
 }
